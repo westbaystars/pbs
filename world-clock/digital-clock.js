e627e199ebd941760198181dc19574030867ea33
Michael Westbay <westbaystars@japanesebaseball.com>
2020-04-10 18:08:59 +0900

    Digital Clock Optimizations
    Cleaned up and optimized digital clock so that it only updates LEDs that have changed values. This was necessary to allow the pulsing colons to not flicker every time they were drawn.

--- world-clock/digital-clock.js
+++ world-clock/digital-clock.js
@@ -1,15 +1,91 @@
 /*
-   The digital clock SVG is from:
+   The digital clock SVG parts are from:
    https://commons.wikimedia.org/wiki/Category:SVG_Digital_clocks
 */
 
-// Call from clock's container element selection
-function dClockCreate(container, width) {
-  var svg = container.append('svg')
+var internalWidth = 250;   // 30 x 6 digits + 15 x 2 colons + 30 x 1 AM/PM indicator
+var internalHeight = 50;
+
+var enterDigits = function(enter) {
+  var g = enter.append("g")
+    .attr("fill","#d00")
+    .attr("stroke","#fff")
+    .attr("stroke-width","2")
+    .attr("value", d => d.digit)
+    .attr("x-offset", d => d.x)
+    .classed("digit", true);
+  
+  // Because the digits are drawn as a set of functions rather than a single append,
+  // we have to kind of bruite force the initial drawing of the digits by getting the
+  // nodes, converting them back to a D3 selection, and using the data from there.
+  g.nodes().forEach(g => {
+    var g = d3.select(g);
+    var ledIndex = g.datum().digit.charCodeAt(0) - 48 // ASCII of "0" is dec 48; {:;<} follow after "9"
+    var offsetX = g.datum().x;
+    g.classed('pulsable',ledIndex === 10)             // Reset if pulsible only for :
+    DIGITS[ledIndex](g, offsetX, 0);                  // Call LED digit drawing function for current datum index
+  });
+};
+
+// Update all of the digits (passed in the "update" parameter)
+var updateDigits = function(update) {
+  // Because the digits are drawn as a set of functions rather than a single append,
+  // we have to kind of bruite force the initial drawing of the digits by getting the
+  // nodes, converting them back to a D3 selection, and using the data from there.
+  update.nodes().forEach(g => {
+    var g = d3.select(g);
+    var digit = g.datum().digit;
+    var ledIndex = digit.charCodeAt(0) - 48 // ASCII of "0" is dec 48; {:;<} follow after "9"
+    var offsetX = g.datum().x;
+    if (digit !== g.attr('value') || offsetX !== Number(g.attr('x-offset'))) {
+      g.attr('value',digit)
+       .attr('x-offset',offsetX)
+       .attr('stroke-width',2)                          // Reset stroke-width (changed in AM/PM render)
+       .attr('style',null);                             // Clear style (just in case it was set previously)
+      g.classed('pulsable',ledIndex === 10)             // Reset if pulsible only for :
+      g.selectAll('.led').remove();
+      DIGITS[ledIndex](g, offsetX, 0);                  // Call LED digit drawing function for current datum index
+    }
+  });
+};
+
+// Draw the digits for all digital clocks
+// @svg - The SVG canvas for drawing the clock
+// @gitits - Contains a string in the form "\d{1,2}:\d{2}[:\d{2}]?[;|<]?"
+//           where ';' is the AM indicator and '<' is PM
+//           All other characters in the string are ignored
+var drawDigits = function (svg, digits) {
+  // Need to keep track of X offset in data
+  var data = [];
+  var offsetX = 0;
+  digits.replace(/[^\d:;<]/g,'')
+    .split('')
+    .map(d => { data.push({ digit: d, x: offsetX }); offsetX += (d === ':')? 15:30; }); // Colon is 15 units wide, otherwise 30
+  
+  // Select all digital clocks for updating
+  svg.selectAll('g.digit')
+    .data(data)
+    .join(
+      enter => enterDigits(enter),
+      update => updateDigits(update),
+      exit => exit.remove()
+    )
+  // Center the clock within the SVG canvas
+  svg.attr('transform','translate('+((250-offsetX)/2)+',0)');
+}
+
+// Digital clock contructor
+// @parent - A D3 selection with data property of a String named "digits"
+// @digits - Contains a string of digits, colons, and optional ; or < for AM or PM indicators
+// @width - The physical screen width for the digital clock
+function createDigitalClock(parent, digits, width) {
+  var svg = parent.append('svg')
     .attr("width", width)
     .attr("height", width/5)
-    .attr("viewBox", "0 0 250 50") // 30 x 6 digits + 15 x 2 colons + 30 x 1 AM/PM
-    .attr("class", "digital-clock");
+    .attr("viewBox", "0 0 "+internalWidth+" "+internalHeight)
+    .classed('digital-clock', true);
+
+  // Define the digital "lit up" element drawing paths (vertical and horizontal)
   var defs = svg.append("defs");
   defs.append("path")
     .attr("id", "vs")
@@ -18,255 +94,186 @@ function dClockCreate(container, width) {
     .attr("id", "hs")
     .attr("d","m0,0 l2.5,2.5 l10.0,0 l2.5,-2.5 l-2.5,-2.5 l-10.0,0 l-2.5,2.5 z");
 
-  container.selectAll('.digital-clock')
-    .call(dClockDrawDigits);
+  drawDigits(svg, digits);
 }
 
-// Draw digits of clock
-// Data should be a string value to the "digits" property in the form of:
-//  \d{1,2}:\d{2}[:\d{2}]?[;|<]? where ; is the AM indicator and < is PM
-function dClockDrawDigits(selection) {
-  // This version of D3 is not working as documented. There is no index variable
-  // passed to each and this is not the current element. Manually tracking the
-  // index and current node.
-  var i=0;
-  selection.each(d => {
-    var svg = d3.select(selection.nodes()[i])
-    var data = d.digits;
-    var offsetX = offsetY = 0;
-    svg.selectAll('.digit').data([]).exit().remove();
-    data.forEach(digit => {
-      var idx = digit.charCodeAt(0) - 48 // ASCII '0' is dec 48; ':;<' follow after '9'
-      if (idx <= 12) {
-        digits[idx](svg, offsetX, offsetY);
-        offsetX += (idx === 10)? 15 : 30;
-      }
-    });
-    // Move the clock right by half the remaining space available
-    svg.attr('transform','translate('+((250-offsetX)/2)+',0)');
-    i++;
-  });
+// Update digital clock with a new time (located in "update" data)
+function updateDigitalClock(update) {
+  // Skip this if not in update phase
+  if (update.data().length === 0) return;
+  
+  drawDigits(update.select('svg.digital-clock'), update.datum().digits);
 }
 
-var clockTop = function(g, offsetX, offfsetY) {
+var ledTop = function(g, offsetX, offsetY) {
   g.append("use")
     .attr("href", "#hs")
     .attr("x", offsetX + 10.0)
-    .attr("y", offsetY + 5.0);
+    .attr("y", offsetY + 5.0)
+    .classed('led',true);
 }
 
-var clockTopLeft = function(g, offsetX, offfsetY) {
+var ledTopLeft = function(g, offsetX, offsetY) {
   g.append("use")
     .attr("href", "#vs")
     .attr("x", offsetX + 5.0)
-    .attr("y", offsetY + 22.5);
+    .attr("y", offsetY + 22.5)
+    .classed('led',true);
 }
 
-var clockTopRight = function(g, offsetX, offfsetY) {
+var ledTopRight = function(g, offsetX, offsetY) {
   g.append("use")
     .attr("href", "#vs")
     .attr("x", offsetX + 20.0)
-    .attr("y", offsetY + 22.5);
+    .attr("y", offsetY + 22.5)
+    .classed('led',true);
 }
 
-var clockMiddle = function(g, offsetX, offfsetY) {
+var ledMiddle = function(g, offsetX, offsetY) {
   g.append("use")
     .attr("href", "#hs")
     .attr("x", offsetX + 7.5)
-    .attr("y", offsetY + 25.0);
+    .attr("y", offsetY + 25.0)
+    .classed('led',true);
 }
 
-var clockBottomLeft = function(g, offsetX, offfsetY) {
+var ledBottomLeft = function(g, offsetX, offsetY) {
   g.append("use")
     .attr("href", "#vs")
     .attr("x", offsetX + 2.5)
-    .attr("y", offsetY + 42.5);
+    .attr("y", offsetY + 42.5)
+    .classed('led',true);
 }
 
-var clockBottomRight = function(g, offsetX, offfsetY) {
+var ledBottomRight = function(g, offsetX, offsetY) {
   g.append("use")
     .attr("href", "#vs")
     .attr("x", offsetX + 17.5)
-    .attr("y", offsetY + 42.5);
+    .attr("y", offsetY + 42.5)
+    .classed('led',true);
 }
 
-var clockBottom = function(g, offsetX, offfsetY) {
+var ledBottom = function(g, offsetX, offsetY) {
   g.append("use")
     .attr("href", "#hs")
     .attr("x", offsetX + 5.0)
-    .attr("y", offsetY + 45.0);
+    .attr("y", offsetY + 45.0)
+    .classed('led',true);
 }
 
 /* All digital clock numbers assume an SVG space of 30 x 50. */
-var clock0 = function(svg, offsetX, offsetY) {
-  var group = svg.append("g")
-    .attr("fill","#d00")
-    .attr("stroke","#fff")
-    .attr("stroke-width","2")
-    .attr("class", "digit");
-  clockTop(group, offsetX, offsetY)
-  clockTopLeft(group, offsetX, offsetY)
-  clockTopRight(group, offsetX, offsetY)
-  clockBottomLeft(group, offsetX, offsetY)
-  clockBottomRight(group, offsetX, offsetY)
-  clockBottom(group, offsetX, offsetY)
+var led0 = function(group, offsetX, offsetY) {
+  ledTop(group, offsetX, offsetY)
+  ledTopLeft(group, offsetX, offsetY)
+  ledTopRight(group, offsetX, offsetY)
+  ledBottomLeft(group, offsetX, offsetY)
+  ledBottomRight(group, offsetX, offsetY)
+  ledBottom(group, offsetX, offsetY)
 }
 
-var clock1 = function(svg, offsetX, offsetY) {
-  var group = svg.append("g")
-    .attr("fill","#d00")
-    .attr("stroke","#fff")
-    .attr("stroke-width","2")
-    .attr("class", "digit");
-  clockTopRight(group, offsetX, offsetY)
-  clockBottomRight(group, offsetX, offsetY)
+var led1 = function(group, offsetX, offsetY) {
+  ledTopRight(group, offsetX, offsetY)
+  ledBottomRight(group, offsetX, offsetY)
 }
 
-var clock2 = function(svg, offsetX, offsetY) {
-  var group = svg.append("g")
-    .attr("fill","#d00")
-    .attr("stroke","#fff")
-    .attr("stroke-width","2")
-    .attr("class", "digit");
-  clockTop(group, offsetX, offsetY)
-  clockTopRight(group, offsetX, offsetY)
-  clockMiddle(group, offsetX, offsetY)
-  clockBottomLeft(group, offsetX, offsetY)
-  clockBottom(group, offsetX, offsetY)
+var led2 = function(group, offsetX, offsetY) {
+  ledTop(group, offsetX, offsetY)
+  ledTopRight(group, offsetX, offsetY)
+  ledMiddle(group, offsetX, offsetY)
+  ledBottomLeft(group, offsetX, offsetY)
+  ledBottom(group, offsetX, offsetY)
 }
 
-var clock3 = function(svg, offsetX, offsetY) {
-  var group = svg.append("g")
-    .attr("fill","#d00")
-    .attr("stroke","#fff")
-    .attr("stroke-width","2")
-    .attr("class", "digit");
-  clockTop(group, offsetX, offsetY)
-  clockTopRight(group, offsetX, offsetY)
-  clockMiddle(group, offsetX, offsetY)
-  clockBottomRight(group, offsetX, offsetY)
-  clockBottom(group, offsetX, offsetY)
+var led3 = function(group, offsetX, offsetY) {
+  ledTop(group, offsetX, offsetY)
+  ledTopRight(group, offsetX, offsetY)
+  ledMiddle(group, offsetX, offsetY)
+  ledBottomRight(group, offsetX, offsetY)
+  ledBottom(group, offsetX, offsetY)
 }
 
-var clock4 = function(svg, offsetX, offsetY) {
-  var group = svg.append("g")
-    .attr("fill","#d00")
-    .attr("stroke","#fff")
-    .attr("stroke-width","2")
-    .attr("class", "digit");
-  clockTopLeft(group, offsetX, offsetY)
-  clockTopRight(group, offsetX, offsetY)
-  clockMiddle(group, offsetX, offsetY)
-  clockBottomRight(group, offsetX, offsetY)
+var led4 = function(group, offsetX, offsetY) {
+  ledTopLeft(group, offsetX, offsetY)
+  ledTopRight(group, offsetX, offsetY)
+  ledMiddle(group, offsetX, offsetY)
+  ledBottomRight(group, offsetX, offsetY)
 }
 
-var clock5 = function(svg, offsetX, offsetY) {
-  var group = svg.append("g")
-    .attr("fill","#d00")
-    .attr("stroke","#fff")
-    .attr("stroke-width","2")
-    .attr("class", "digit");
-  clockTop(group, offsetX, offsetY)
-  clockTopLeft(group, offsetX, offsetY)
-  clockMiddle(group, offsetX, offsetY)
-  clockBottomRight(group, offsetX, offsetY)
-  clockBottom(group, offsetX, offsetY)
+var led5 = function(group, offsetX, offsetY) {
+  ledTop(group, offsetX, offsetY)
+  ledTopLeft(group, offsetX, offsetY)
+  ledMiddle(group, offsetX, offsetY)
+  ledBottomRight(group, offsetX, offsetY)
+  ledBottom(group, offsetX, offsetY)
 }
 
-var clock6 = function(svg, offsetX, offsetY) {
-  var group = svg.append("g")
-    .attr("fill","#d00")
-    .attr("stroke","#fff")
-    .attr("stroke-width","2")
-    .attr("class", "digit");
-  clockTop(group, offsetX, offsetY)
-  clockTopLeft(group, offsetX, offsetY)
-  clockMiddle(group, offsetX, offsetY)
-  clockBottomLeft(group, offsetX, offsetY)
-  clockBottomRight(group, offsetX, offsetY)
-  clockBottom(group, offsetX, offsetY)
+var led6 = function(group, offsetX, offsetY) {
+  ledTop(group, offsetX, offsetY)
+  ledTopLeft(group, offsetX, offsetY)
+  ledMiddle(group, offsetX, offsetY)
+  ledBottomLeft(group, offsetX, offsetY)
+  ledBottomRight(group, offsetX, offsetY)
+  ledBottom(group, offsetX, offsetY)
 }
 
-var clock7 = function(svg, offsetX, offsetY) {
-  var group = svg.append("g")
-    .attr("fill","#d00")
-    .attr("stroke","#fff")
-    .attr("stroke-width","2")
-    .attr("class", "digit");
-  clockTop(group, offsetX, offsetY)
-  clockTopRight(group, offsetX, offsetY)
-  clockBottomRight(group, offsetX, offsetY)
+var led7 = function(group, offsetX, offsetY) {
+  ledTop(group, offsetX, offsetY)
+  ledTopRight(group, offsetX, offsetY)
+  ledBottomRight(group, offsetX, offsetY)
 }
 
-var clock8 = function(svg, offsetX, offsetY) {
-  var group = svg.append("g")
-    .attr("fill","#d00")
-    .attr("stroke","#fff")
-    .attr("stroke-width","2")
-    .attr("class", "digit");
-  clockTop(group, offsetX, offsetY)
-  clockTopLeft(group, offsetX, offsetY)
-  clockTopRight(group, offsetX, offsetY)
-  clockMiddle(group, offsetX, offsetY)
-  clockBottomLeft(group, offsetX, offsetY)
-  clockBottomRight(group, offsetX, offsetY)
-  clockBottom(group, offsetX, offsetY)
+var led8 = function(group, offsetX, offsetY) {
+  ledTop(group, offsetX, offsetY)
+  ledTopLeft(group, offsetX, offsetY)
+  ledTopRight(group, offsetX, offsetY)
+  ledMiddle(group, offsetX, offsetY)
+  ledBottomLeft(group, offsetX, offsetY)
+  ledBottomRight(group, offsetX, offsetY)
+  ledBottom(group, offsetX, offsetY)
 }
 
-var clock9 = function(svg, offsetX, offsetY) {
-  var group = svg.append("g")
-    .attr("fill","#d00")
-    .attr("stroke","#fff")
-    .attr("stroke-width","2")
-    .attr("class", "digit");
-  clockTop(group, offsetX, offsetY)
-  clockTopLeft(group, offsetX, offsetY)
-  clockTopRight(group, offsetX, offsetY)
-  clockMiddle(group, offsetX, offsetY)
-  clockBottomRight(group, offsetX, offsetY)
-  clockBottom(group, offsetX, offsetY)
+var led9 = function(group, offsetX, offsetY) {
+  ledTop(group, offsetX, offsetY)
+  ledTopLeft(group, offsetX, offsetY)
+  ledTopRight(group, offsetX, offsetY)
+  ledMiddle(group, offsetX, offsetY)
+  ledBottomRight(group, offsetX, offsetY)
+  ledBottom(group, offsetX, offsetY)
 }
 
 /* Digital clock colon (:) assumes an SVG space of 15 x 50. */
-var clockColon = function(svg, offsetX, offsetY) {
-  var group = svg.append("g")
-    .attr("fill","#d00")
-    .attr("stroke","#fff")
-    .attr("stroke-width","2")
-    .attr("class", "digit pulsing");
+var ledColon = function(group, offsetX, offsetY) {
   group.append("path")
     .attr("d", "m 12.5,17.5 a 3.86,3.78 0 1 1 0,-.01z")
     .attr("transform", "translate("+offsetX+","+offsetY+")")
+    .classed('led',true);
   group.append("path")
     .attr("d", "m 12.5,17.5 a 3.86,3.78 0 1 1 0,-.01z")
     .attr("transform", "translate("+(offsetX-2.28)+","+(offsetY+17.5)+")")
+    .classed('led',true);
 }
 
-var clockAM = function(svg, offsetX, offsetY) {
-  var group = svg.append("g")
-    .attr("fill","#d00")
-    .attr("stroke","#fff")
-    .attr("stroke-width", 0.1)
-    .attr("style","text-anchor:end;font-size:20px;font-family:Arial")
-    .attr("class", "digit");
+var ledAM = function(group, offsetX, offsetY) {
+  group
+    .attr('stroke-width',0.1)
+    .attr('style','text-anchor:end;font-size:20px;font-family:Arial;');
   group.append("text")
     .attr("x", offsetX + 30.0)
     .attr("y", offsetY + 22.5)
+    .classed('led',true)
     .text("AM");
 }
 
-var clockPM = function(svg, offsetX, offsetY) {
-  var group = svg.append("g")
-    .attr("fill","#d00")
-    .attr("stroke","#fff")
-    .attr("stroke-width", 0.1)
-    .attr("style","text-anchor:end;font-size:20px;font-family:Arial")
-    .attr("class", "digit");
+var ledPM = function(group, offsetX, offsetY) {
+  group
+    .attr('stroke-width',0.1)
+    .attr('style','text-anchor:end;font-size:20px;font-family:Arial;');
   group.append("text")
     .attr("x", offsetX + 26.6)
     .attr("y", offsetY + 45.0)
+    .classed('led',true)
     .text("PM");
 }
 
 // Clock digit drawing functions
-const digits = [clock0, clock1, clock2, clock3, clock4, clock5, clock6, clock7, clock8, clock9, clockColon, clockAM, clockPM];
+const DIGITS = [led0, led1, led2, led3, led4, led5, led6, led7, led8, led9, ledColon, ledAM, ledPM];
--- world-clock/index.js
+++ world-clock/index.js
@@ -1,5 +1,10 @@
-/* Script to run the World Clock web app page */
-/* Assumes clock.js has been imported with object zones available as data souce */
+/* Script to run the World Clock web app page
+   Assumes digital-clock.js and analog-clock.js have been imported
+
+   Data drives the creation of cards on the page. Each card contains a time zone name, the time
+   in a string format for display on a digital clock, and sequence of hours, minutes, seconds in
+   D3 friendly unit/number object pairs for an analog clock.
+*/
 
 // Store the current settings, time, and time zones into a D3 friendly data structure
 const settings = {
@@ -19,12 +24,12 @@ const settings = {
     var timeFormat = '' + (this.show24? 'HH':'h') + ':mm' + (this.showSeconds? ':ss':'') + (this.show24?  '':'A');
 
     this.currentTime = new moment().utc();
-    this.pulseLevel = ((this.currentTime.seconds() % 4) * 30);
+    this.pulseLevel = ((this.currentTime.seconds() % 2) * 1 + .1);  // Opacity goes from 0 to 1 (or more)
     this.timeZones.forEach((tz, i) => {
       var m = this.currentTime.tz(tz)
       d.push({
         zone: tz,
-        digits: m.format(timeFormat).replace('AM',';').replace('PM','<').split(''),
+        digits: m.format(timeFormat).replace('AM',';').replace('PM','<'),
         time: [
           {units: "hours", number: m.hours() + m.minutes() / 60}, // Includining minutes allows hour hand to move between hours
           {units: "minutes", number: m.minutes()},
@@ -37,7 +42,12 @@ const settings = {
   init: () => {
     d3.select('#show24Hour').on('change', () => { settings.show24 = d3.select('#show24Hour').property('checked'); });
     d3.select('#showSeconds').on('change', () => { settings.showSeconds = d3.select('#showSeconds').property('checked'); });
-    d3.select('#pulsing').on('change', () => { settings.showPulsing = d3.select('#pulsing').property('checked'); });
+    d3.select('#pulsing').on('change', () => {
+      settings.showPulsing = d3.select('#pulsing').property('checked');
+      // Reset to full opacity when toggled to start
+      d3.selectAll('.pulsable')
+        .style("opacity", 1);
+     });
   }
 }
 settings.init();
@@ -45,6 +55,10 @@ settings.init();
 var container = d3.select('#container');
 
 function createCard(enter) {
+  // Don't do anything if there is no data attached to the enter object (already created)
+  if (enter.data().length === 0) return;
+  
+  var data = enter.datum();
   var timeCards = enter.append('div')
       .attr('class','card time-card')
       .style('width','15rem')
@@ -54,7 +68,7 @@ function createCard(enter) {
   var body = timeCards.append('div')
     .attr('class', 'card-body');
   aClockCreate(body, 200);
-  dClockCreate(body, 200);
+  createDigitalClock(body, data.digits, 200);
 }
 
 function updateCard(update) {
@@ -62,10 +76,9 @@ function updateCard(update) {
     .text(d => d.zone);
   update.selectAll('.card-body .analog-clock .clock-group')
     .call(aClockDrawHands);
-  update.selectAll('.card-body .digital-clock')
-    .call(dClockDrawDigits);
+  update.call(updateDigitalClock);
   if (settings.showPulsing) {
-    d3.selectAll('.pulsing')
+    d3.selectAll('.pulsable')
       .transition()
       .style("opacity", settings.pulseLevel);
   }
@@ -81,7 +94,7 @@ function render(data) {
         // Make sure data is propogated to children
         container.selectAll('.time-card').data(data).select('.card-header');
         container.selectAll('.time-card').data(data).select('.card-body .analog-clock .clock-group');
-        container.selectAll('.time-card').data(data).select('.card-body .digital-clock');
+        //container.selectAll('.time-card').data(data).select('.card-body .digital-clock');
       },
       update => updateCard(update),
       exit => exit.remove()
@@ -97,8 +110,9 @@ setInterval(function() {
 
 setInterval(function() {
   if (settings.showPulsing) {
-    d3.selectAll('.pulsing')
+    d3.selectAll('.pulsable')
     .transition()
     .style("opacity", settings.pulseLevel);
   }
-}, 2000)
+}, 1000)
+
