efd4f38406991a82436ae792a463314e4c624e2a
Michael Westbay <westbaystars@gmail.com>
2021-10-19 18:48:10 +0900

    Implemented the Sleeps to Christmas assignment.

--- sleeps_to_christmas/lib/sleeps_to_christmas.ex
+++ sleeps_to_christmas/lib/sleeps_to_christmas.ex
@@ -1,4 +1,6 @@
 defmodule SleepsToChristmas do
+  import Date
+
   @moduledoc """
   Documentation for `SleepsToChristmas`.
   """
@@ -12,7 +14,28 @@ defmodule SleepsToChristmas do
       :world
 
   """
-  def hello do
-    :world
+  def sleeps do
+    days = days_to_next_christmas()
+    out = case days do
+      0 -> "No more sleeps — it's Christmas 😀🎄🎁"
+      1 -> "The sleep 😴 before Christmas 🎄"
+      _ -> "#{days} sleeps 😴 till Christmas 🎄"
+    end
+    IO.puts(out)
+  end
+
+  defp days_to_next_christmas() do
+    today = local_date()
+    christmas = new!(today.year, 12, 25)
+    cond do
+      :gt == compare(christmas, today) -> christmas
+      true -> new!(today.year + 1, 12, 25)
+    end
+    day_of_year(christmas) - day_of_year(today)
+  end
+
+  defp local_date() do
+    {erl_date, _erl_time} = :calendar.local_time()
+    Date.from_erl!(erl_date)
   end
 end
