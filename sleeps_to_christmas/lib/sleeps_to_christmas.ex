7256f6bdac5db5effde852900dd18494ccb96f6a
Michael Westbay <westbaystars@gmail.com>
2021-10-24 18:13:18 +0900

    Made days_to_next_christmas function public so that it could be tested. Also rewrote function to use pattern matching instead of cond block.

--- sleeps_to_christmas/lib/sleeps_to_christmas.ex
+++ sleeps_to_christmas/lib/sleeps_to_christmas.ex
@@ -15,23 +15,23 @@ defmodule SleepsToChristmas do
 
   """
   def sleeps do
-    days_to_next_christmas()
-    |> phrase()
-  end
-
-  defp days_to_next_christmas() do
-    today = local_date()
-    christmas = new!(today.year, 12, 25)
-    cond do
-      :gt == compare(christmas, today) -> christmas
-      true -> new!(today.year + 1, 12, 25)
-    end
-    day_of_year(christmas) - day_of_year(today)
+    local_date()                  # Get the current date
+    |> days_to_next_christmas()   # Calculate the number of days until Christmas
+    |> phrase()                   # Generate phrase
   end
 
   defp phrase(0), do: "No more sleeps — it's Christmas 😀🎄🎁"
   defp phrase(1), do: "The sleep 😴 before Christmas 🎄"
   defp phrase(days), do: "#{days} sleeps 😴 till Christmas 🎄"
+
+  def days_to_next_christmas(date) do
+    christmas = new!(date.year, 12, 25)
+    christmas = add_year_if_before(christmas, compare(christmas, date))
+    day_of_year(christmas) - day_of_year(date)
+  end
+
+  defp add_year_if_before(date, :gt), do: date
+  defp add_year_if_before(date, _), do: new!(date.year + 1, date.month, date.day)
 
   defp local_date() do
     {erl_date, _erl_time} = :calendar.local_time()
